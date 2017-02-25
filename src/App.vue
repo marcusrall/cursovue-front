6b3d759108fdbc603ef8d69fe537bee11be0cdcf
marcusrall <marcus.almeira@webrall.com>
2017-02-25 18:11:34 -0300

    Criando os componentes Login e Register/ Importando eles

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="container" id="app">
       <navbar></navbar>
+      <login></login>
+      <register></register>
   </div>
 </template>
 
 <script>
   import Navbar from './components/Navbar.vue'
+  import Login from './components/authentication/Login.vue'
+  import Register from './components/authentication/Register.vue'
+
 
   export default {
     components: {
-      'navbar': Navbar
+      'navbar': Navbar,
+      'login': Login,
+      'register': Register
     }
   }
 </script>
---
+++ src/components/authentication/Login.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+
+          <div class="panel-body">
+            <div class="form-group">
+              <input v-model="email" class="form-control" type="email" placeholder="Email">
+            </div>
+            <div class="form-group">
+              <input v-model="password" class="form-control" type="password" placeholder="Password">
+            </div>
+            <button class="btn btn-success pull-right">Login</button>
+          </div>
+
+        </div>
+      </div>
+      <pre>
+        {{ $data }}
+      </pre>
+    </div>
+</template>
+
+<script>
+    export default {
+      data(){
+        return {
+          email:'',
+          password: ''
+        }
+      }
+    }
+</script>
+
+<style lang="css">
+</style>
