5e4a4bce7aaf65e5560d6a2988d256730e748a6f
marcusrall <marcus.almeira@webrall.com>
2017-02-25 18:54:19 -0300

    Instalação vue-router e configuração

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <template>
   <div class="container" id="app">
       <navbar></navbar>
-      <login></login>
-      <register></register>
+      <router-view></router-view>
   </div>
 </template>
 
 <script>
   import Navbar from './components/Navbar.vue'
-  import Login from './components/authentication/Login.vue'
-  import Register from './components/authentication/Register.vue'
+
 
 
   export default {
     components: {
       'navbar': Navbar,
-      'login': Login,
-      'register': Register
+    
     }
   }
 </script>
