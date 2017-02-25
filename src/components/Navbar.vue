9ebdb009e4e67c6f9271a3bc8b4964b630ffbff5
marcusrall <marcus.almeira@webrall.com>
2017-02-25 17:52:06 -0300

    criando e importando um component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div>
+    <div class="header clearfix">
+      <nav>
+        <ul class="nav nav-pills pull-right">
+          <li role="presentation" class="active"><a href="#">Home</a></li>
+          <li role="presentation"><a href="#">About</a></li>
+          <li role="presentation"><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+      <h3 class="text-muted">Project name</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
